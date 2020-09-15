b2cb4150cfadd8d8d70a7822a03a9e3ec6b7f80c
oabanin-HOME <juristaba@gmail.com>
2020-09-16 00:18:02 +0300

    add banner section

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -5,14 +5,18 @@ const Banner = () => {
 		<section className="banner">
 			<div className="container-fluid">
 				<div className="row">
-					<div className="col-lg-7 w-50">
-						<h1 className="banner__title">
-						</h1>
+					<div className="col-lg-7">
+						<h1 className="banner__title">Test assignment for Frontend Developer position</h1>
+						<p className="banner__text">
+							We kindly remind you that your test assignment should be submitted as a link to github/bitbucket repository. Please be patient, we consider and respond to every application that meets minimum requirements. We look forward to your submission. Good luck! The photo has to scale in the banner area on the different screens
+						</p>
+						<a className="button" href="#sign-up">Sing up now</a>
 					</div>
 				</div>
+				
 			</div>
 		</section>
-		)
+	)
 }
 
 export default Banner;
