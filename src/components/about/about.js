70356225d04bf2c52b9e13f09a6011ddacfb805b
oabanin-HOME <juristaba@gmail.com>
2020-09-17 00:19:53 +0300

    finished desktop about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,13 +6,22 @@ const About = () => {
 		<section className="about">
 			<div className="container-fluid">
 				<div className="row">
-					<div className="col-lg-6 offset-lg-3">
+					<div className="col-md-8 offset-md-2">
 						<h1 className="about__title">Let's get acquainted</h1>
 					</div>
 				</div>
 				<div className="row">
 					<div className="col-lg-5">
 						<img className="about__logo" src={AboutLogo} />
+					</div>
+					<div className="col-lg-7">
+						<div className="about__text">
+							<h2 className="about__heading">I am cool frontend developer</h2>
+							<p>We will evaluate how clean your approach to writing CSS and Javascript code is. You can use any CSS and Javascript 3rd party libraries without any restriction.</p>
+							<p>If 3rd party css/javascript libraries are added to the project via bower/npm/yarn you will get bonus points. If you use any task runner (gulp/webpack) you will get bonus points as well. Slice service directory page P​SD mockup​ into HTML5/CSS3. </p>
+
+							<a className="about__link" href="#sign-up">Sing up now</a>
+						</div>
 					</div>
 				</div>
 			</div>
