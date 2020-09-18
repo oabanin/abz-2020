ddfe5d2335e49f466eb5739fc887e752ba469f27
oabanin-HOME <juristaba@gmail.com>
2020-09-18 20:39:35 +0300

    finished draft of user component

--- src/components/users/components/user/user.js
+++ src/components/users/components/user/user.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
 
 const User = ({photo, name, job, mail, phone}) => {
+
+  useEffect(() => compareSize(), []);
+
+  const node = useRef();
+
+  const [isTooltipDisable, setTooltipDisable] = useState(true);
+
+  const compareSize = () => {
+    const scrollWidthLargerClientWidth = node.current.scrollWidth > node.current.clientWidth;
+    setTooltipDisable(!scrollWidthLargerClientWidth);
+  };
+
   return(
      <div className="col-md-4">
             <img className="users__photo" src={photo}/>
             <div className="users__info">
-              <h2 className="users__name">Maximillian</h2>
-              <div className="users__job">Leading specialist of the Control Department</div>
-
-              <a data-tip="controldepartment@gmail" className="users__email" href="mailto:controldepartment@gmail">controldepartment@gmail</a>
-              <div className="users__phone">+380 50 678 03 24</div>
+              <h2 className="users__name">{name}</h2>
+              <div className="users__job">{job}</div>
+              <a ref={node} data-tip-disable={isTooltipDisable} data-tip={mail} className="users__email" href={`mailto:${mail}`}>{mail}</a>
+              <div className="users__phone">{phone}</div>
             </div>
       </div>
     );
--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,47 +1,50 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import ReactTooltip from 'react-tooltip';
 import User from './components/user';
 
-import Photo from './photo.jpg';
+//import Photo from './photo.jpg';
 
 const Users = () => {
-  const users = [
+
+  const usersDB = [
     {
       id: 1,
-      photo: "https:\//frontend-test-assignment-api.abz.agency/images/users/5e54ff59e5a4548.jpeg",
+      photo: "https://frontend-test-assignment-api.abz.agency/images/users/5e54ff59e5a4548.jpeg",
       name: "Maximillian",
       job: "Leading specialist of the Control Department",
       mail: "controldepartment@gmail",
       phone: "+380 50 678 03 24"
+    },
+    {
+      id: 2,
+      photo: "https://frontend-test-assignment-api.abz.agency/images/users/5e54ff59e5a4548.jpeg",
+      name: "Adolph Blaine Charles David Earl Matthew Matthew",
+      job: "Contextual advertising specialist",
+      mail: "adolph.blainecharles@gmail.com",
+      phone: "+380 50 678 03 24"
+    },
+    {
+      id: 3,
+      photo: "https://frontend-test-assignment-api.abz.agency/images/users/5e54ff59e5a4548.jpeg",
+      name: "Adolph Blaine Charles David Earl Matthew Matthew",
+      job: "Contextual advertising specialist",
+      mail: "adolph.blainecharles@gmail.com",
+      phone: "+380 50 678 03 24"
+    },
+    {
+      id: 4,
+      photo: "https://frontend-test-assignment-api.abz.agency/images/users/5e54ff59e5a4548.jpeg",
+      name: "Adolph Blaine Charles David Earl Matthew Matthew",
+      job: "Contextual advertising specialist",
+      mail: "adolph.blainecharles@gmail.com",
+      phone: "+380 50 678 03 24"
     }
   ];
 
-
-
-
-
-
-  // useEffect(() => {
-  //   compareSize();
-  // }, []);
-
-  // const anode = useRef();
-
-  // const [isInvisible, setInvisibility] = useState(true);
-
-  // const compareSize = () => {
-  //   const compare =
-  //     anode.current.scrollWidth > anode.current.clientWidth;
-  //   console.log('compare: ', compare);
-  //   setInvisibility(!compare);
-  // };
-
-  // console.log(anode);
-  //compareSize();
+  const users = usersDB.map(({id, ...userInfo}) => <User key={id} {...userInfo} />);
 
 	return (
 		<section className="users">
-      <ReactTooltip place="bottom" />
 			<div className="container-fluid">
 
 				<div className="row">
@@ -57,7 +60,8 @@ const Users = () => {
         </div>
 
         <div className="row">
-
+        <ReactTooltip place="bottom" />
+          {users}
         </div>
 
 			</div>
@@ -66,27 +70,3 @@ const Users = () => {
 }
 
 export default Users;
-
- // <div className="col-md-4">
- //            <img className="users__photo" src={Photo}/>
- //            <div className="users__info">
- //              <h2 className="users__name">Maximillian</h2>
- //              <div className="users__job">Leading specialist of the Control Department</div>
-
- //              <a data-tip="controldepartment@gmail" className="users__email" href="mailto:controldepartment@gmail">controldepartment@gmail</a>
- //              <div className="users__phone">+380 50 678 03 24</div>
- //            </div>
- //          </div>
- //          <div className="col-md-4">
- //            <img className="users__photo" src={Photo}/>
- //            <div className="users__info">
- //              <h2 className="users__name">Adolph Blaine Charles David Earl Matthew Matthew</h2>
- //              <div className="users__job">Contextual advertising specialist</div>
-
- //              <a ref={anode} data-tip-disable={isInvisible} data-tip="adolph.blainecharles@gmail.com" className="users__email" href="mailto:adolph.blainecharles@gmail.com">adolph.blainecharles@gmail.com</a>
- //              <div className="users__phone">+380 50 678 03 24</div>
- //            </div>
- //          </div>
- //          <div className="col-md-4">
- //            <img className="users__photo" src={Photo}/>
- //          </div>
