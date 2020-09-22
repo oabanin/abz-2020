d6cef7a7af173dc392c1c05f90a4642081ff2321
oabanin-HOME <juristaba@gmail.com>
2020-09-22 19:42:02 +0300

    add footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div>
-			Footer
-		</div>
+		<footer className="footer">
+			<hr/>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-md-4 offset-md-4">
+						<div className="footer__copyright">© abz.agency specially for the test task</div>
+					</div>
+				</div>
+			</div>
+		</footer>
 	)
 }
 
