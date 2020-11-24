d6aba200250d6d6dfd88933eed8bb1a1ed09de87
oabanin-HOME <juristaba@gmail.com>
2020-11-25 00:27:22 +0300

    begin 360 pixel perfect

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ const Header = () => {
 		<header className="header">
 			<div className="container-fluid">
 				<div className="row h-100">
-					<div className="col-lg-2 col-md-2 col-sm-2 d-flex align-items-center">
+					<div className="col-lg-2 col-md-2 col-sm-6 col-6 d-flex align-items-center">
 						<a
 							className="header__logo"
 							onClick={() => animateScroll.scrollToTop()}
@@ -21,7 +21,7 @@ const Header = () => {
 							/>
 						</a>
 					</div>
-					<div className="col-lg-6 offset-lg-4 col-md-8 offset-md-2 col-sm-1 offset-sm-9">
+					<div className="col-lg-6 offset-lg-4 col-md-8 offset-md-2 col-sm-6 col-6">
 						<Menu />
 						<div className="header__burger">
 							<img
