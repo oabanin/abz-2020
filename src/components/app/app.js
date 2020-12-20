04349727420b2d442730c8a2787ef73d83c6f268
oabanin-HOME <juristaba@gmail.com>
2020-12-21 00:07:43 +0300

    add bundle-loader

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,17 @@ import React from 'react';
 import ErrorBoundary from '../error-boundary';
 import Header from '../header';
 import Banner from '../banner';
-import About from '../about';
-import Users from '../users';
-import Form from '../form';
-import Footer from '../footer';
+// import About from '../about';
+// import Users from '../users';
+// import Form from '../form';
+// import Footer from '../footer';
+
+import About from 'bundle-loader?lazy!../about';
+import Users from 'bundle-loader?lazy!../users';
+import Form from 'bundle-loader?lazy!../form';
+import Footer from 'bundle-loader?lazy!../footer';
+
+
 
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { Provider } from 'react-redux'
 import ReactModal from 'react-modal';
 
 import App from './components/app';
+
+//import LoadHome from 'bundle-loader?lazy!./pages/home';
+
+
 import ErrorBoundary from './components/error-boundary';
 import { ApiServiceProvider } from './components/api-service-context';
 import * as api from './services/api';
@@ -14,13 +18,13 @@ import './scss/styles.scss';
 
 ReactModal.setAppElement('#root');
 ReactDOM.render(
-  <React.StrictMode>
-  	<Provider store={store}>
-  	  <ErrorBoundary>
-  	    <ApiServiceProvider value={api}>
-  	      <App/>
-  	    </ApiServiceProvider>
-  	  </ErrorBoundary>
-  	</Provider>
-  </React.StrictMode>,
-    document.getElementById('root'));
+	<React.StrictMode>
+		<Provider store={store}>
+			<ErrorBoundary>
+				<ApiServiceProvider value={api}>
+					<App />
+				</ApiServiceProvider>
+			</ErrorBoundary>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root'));
