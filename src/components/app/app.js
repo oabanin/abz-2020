c4ef986aebf1a37640344c53057781118c58cb84
oabanin-HOME <juristaba@gmail.com>
2020-12-22 10:53:19 +0300

    changing code splitting

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,13 @@
 import React, { Suspense } from 'react';
 
 import ErrorBoundary from '../error-boundary';
-//import Header from '../header';
-//import Banner from '../banner';
+import Header from '../header';
+import Banner from '../banner';
+import About from '../about';
 import Spinner from '../spinner';
 
-//import About from '../about';
-//import Users from '../users';
-//import Form from '../form';
-
-
-//import Footer from '../footer';
-
-const Header = React.lazy(() => import('../header'));
-const Banner = React.lazy(() => import('../banner'));
 const Form = React.lazy(() => import('../form'));
-
 const Users = React.lazy(() => import('../users'));
-const About = React.lazy(() => import('../about'));
 const Footer = React.lazy(() => import('../footer'));
 
 
@@ -27,16 +17,10 @@ const Footer = React.lazy(() => import('../footer'));
 const App = () => {
 	return (
 		<>
-			<Suspense fallback={<Spinner />}>
-				<Header />
-			</Suspense>
+			<Header />
 			<main>
-				<Suspense fallback={<Spinner />}>
-					<Banner />
-				</Suspense>
-				<Suspense fallback={<Spinner />}>
-					<About />
-				</Suspense>
+				<Banner />
+				<About />
 				<ErrorBoundary>
 					<Suspense fallback={<Spinner />}>
 						<Users />
